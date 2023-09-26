33ae1b923be87f455039ea08d9cea011b1c17d0d
LucasBortoloti <bortoloti91@gmail.com>
2023-09-26 10:19:36 -0300

    26/09/2023

--- init.php
+++ init.php
@@ -1,11 +1,9 @@
 <?php
-if (version_compare(PHP_VERSION, '7.1.0') == -1)
-{
-    die ('The minimum version required for PHP is 7.1.0');
+if (version_compare(PHP_VERSION, '7.1.0') == -1) {
+    die('The minimum version required for PHP is 7.1.0');
 }
 
-if (!file_exists('app/config/application.ini'))
-{
+if (!file_exists('app/config/application.ini')) {
     die('Application configuration file not found');
 }
 
@@ -20,8 +18,8 @@ $loader->register();
 // read configurations
 $ini = parse_ini_file('app/config/application.ini', true);
 date_default_timezone_set($ini['general']['timezone']);
-AdiantiCoreTranslator::setLanguage( $ini['general']['language'] );
-ApplicationTranslator::setLanguage( $ini['general']['language'] );
+AdiantiCoreTranslator::setLanguage($ini['general']['language']);
+ApplicationTranslator::setLanguage($ini['general']['language']);
 AdiantiApplicationConfig::load($ini);
 AdiantiApplicationConfig::apply();
 
@@ -32,6 +30,6 @@ define('PATH', dirname(__FILE__));
 define('LANG', $ini['general']['language']);
 
 // custom session name
-session_name('PHPSESSID_'.$ini['general']['application']);
+session_name('PHPSESSID_' . $ini['general']['application']);
 
 setlocale(LC_ALL, 'C');
