b3654e819e248be25e4e74ffe66bdd02e28d2ddb
LucasBortoloti <bortoloti91@gmail.com>
2023-12-13 11:05:54 -0300

    13/12

--- app/control/apresentacao/CantorList.class.php
+++ app/control/apresentacao/CantorList.class.php
@@ -13,17 +13,17 @@ class CantorList extends TPage
     protected $pageNavigation;
     protected $formgrid;
     protected $deleteButton;
-    
+
     use Adianti\base\AdiantiStandardListTrait;
-    
+
     /**
      * Page constructor
      */
     public function __construct()
     {
-       
+
         parent::__construct();
-        
+
         $this->setDatabase('spotify');            // defines the database
         $this->setActiveRecord('Cantor');   // defines the active record
         $this->setDefaultOrder('id', 'asc');         // defines the default order
@@ -35,88 +35,88 @@ class CantorList extends TPage
         $this->addFilterField('id', '=', 'id');
         $this->addFilterField('nome', '=', 'nome');
         $this->addFilterField('idade', '=', 'idade');
- 
 
-        
+
+
         // creates the form
         $this->form = new BootstrapFormBuilder('form_search_Cantor');
         $this->form->setFormTitle('Cantor');
-        
+
 
         // create the form fields 
         $id = new TEntry('id');
         $nome = new TEntry('nome');
         $idade = new TEntry('idade');
- 
- 
-        // add the fields 
-        $this->form->addFields( [ new TLabel('Id')] , [ $id ] );
-        $this->form->addFields( [ new TLabel('Nome')] , [ $nome ] );
-        $this->form->addFields( [ new TLabel('Idade')] , [ $idade ] );
- 
 
-        
+
+        // add the fields 
+        $this->form->addFields([new TLabel('Id')], [$id]);
+        $this->form->addFields([new TLabel('Nome')], [$nome]);
+        $this->form->addFields([new TLabel('Idade')], [$idade]);
+
+
+
         // keep the form filled during navigation with session data
-        $this->form->setData( TSession::getValue(__CLASS__.'_filter_data') );
-        
+        $this->form->setData(TSession::getValue(__CLASS__ . '_filter_data'));
+
         // add the search form actions
         $btn = $this->form->addAction(_t('Find'), new TAction([$this, 'onSearch']), 'fa:search');
         $btn->class = 'btn btn-sm btn-primary';
         $this->form->addActionLink(_t('New'), new TAction(['CantorForm', 'onEdit']), 'fa:plus green');
-        
+
         // creates a Datagrid
         $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
         $this->datagrid->style = 'width: 100%';
         $this->datagrid->datatable = 'true';
         // $this->datagrid->enablePopover('Popover', 'Hi <b> {name} </b>');
-        
+
 
         // creates the datagrid columns 
         $column_id = new TDataGridColumn('id', 'Id', 'left');
         $column_nome = new TDataGridColumn('nome', 'Nome', 'left');
         $column_idade = new TDataGridColumn('idade', 'Idade', 'left');
- 
- 
+
+
         // add the columns to the DataGrid 
         $this->datagrid->addColumn($column_id);
         $this->datagrid->addColumn($column_nome);
         $this->datagrid->addColumn($column_idade);
- 
 
-        
-        $action1 = new TDataGridAction(['CantorForm', 'onEdit'], ['id'=>'{id}']);
-        $action2 = new TDataGridAction([$this, 'onDelete'], ['id'=>'{id}']);
-        
+
+
+        $action1 = new TDataGridAction(['CantorForm', 'onEdit'], ['id' => '{id}']);
+        $action2 = new TDataGridAction([$this, 'onDelete'], ['id' => '{id}']);
+
         $this->datagrid->addAction($action1, _t('Edit'),   'far:edit blue');
-        $this->datagrid->addAction($action2 ,_t('Delete'), 'far:trash-alt red');
-        
+        $this->datagrid->addAction($action2, _t('Delete'), 'far:trash-alt red');
+
         // create the datagrid model
         $this->datagrid->createModel();
-        
+
         // creates the page navigation
         $this->pageNavigation = new TPageNavigation;
         $this->pageNavigation->setAction(new TAction([$this, 'onReload']));
-        
+
         $panel = new TPanelGroup('', 'white');
         $panel->add($this->datagrid);
         $panel->addFooter($this->pageNavigation);
-        
+
         // header actions
         $dropdown = new TDropDown(_t('Export'), 'fa:list');
         $dropdown->setPullSide('right');
         $dropdown->setButtonClass('btn btn-default waves-effect dropdown-toggle');
-        $dropdown->addAction( _t('Save as CSV'), new TAction([$this, 'onExportCSV'], ['register_state' => 'false', 'static'=>'1']), 'fa:table blue' );
-        $dropdown->addAction( _t('Save as PDF'), new TAction([$this, 'onExportPDF'], ['register_state' => 'false', 'static'=>'1']), 'far:file-pdf red' );
-        $panel->addHeaderWidget( $dropdown );
-        
+        $dropdown->addAction(_t('Save as CSV'), new TAction([$this, 'onExportCSV'], ['register_state' => 'false', 'static' => '1']), 'fa:table blue');
+        $dropdown->addAction(_t('Save as PDF'), new TAction([$this, 'onExportPDF'], ['register_state' => 'false', 'static' => '1']), 'far:file-pdf red');
+        $panel->addHeaderWidget($dropdown);
+
         // vertical box container
         $container = new TVBox;
         $container->style = 'width: 100%';
         // $container->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
         $container->add($this->form);
         $container->add($panel);
-        
-       
+
+
         parent::add($container);
     }
 }
--- app/control/apresentacao/EditorImagem.class.php
+++ app/control/apresentacao/EditorImagem.class.php
@@ -5,36 +5,36 @@ use Adianti\Control\TPage;
 class EditorImagem extends TPage
 {
     private $form;
-    
+
     /**
      * Page constructor
      */
     function __construct()
     {
         parent::__construct();
-        
+
         // create the form
         $this->form = new BootstrapFormBuilder;
-        $this->form->setFormTitle( ('Editor de Imagem') );
-        
+        $this->form->setFormTitle(('Editor de Imagem'));
+
         // create the form fields
         $imagecropper = new TImageCropper('imagecropper');
         $imageupload  = new TFile('imageupload');
-        
+
         $imagecropper->setSize(300, 150);
         $imagecropper->setCropSize(300, 150);
-        $imagecropper->setAllowedExtensions( ['gif', 'png', 'jpg', 'jpeg'] );
+        $imagecropper->setAllowedExtensions(['gif', 'png', 'jpg', 'jpeg']);
         //$imagecropper->enableFileHandling();
-        
-        $imageupload->setAllowedExtensions( ['gif', 'png', 'jpg', 'jpeg'] );
+
+        $imageupload->setAllowedExtensions(['gif', 'png', 'jpg', 'jpeg']);
         //$imageupload->enableFileHandling();
         $imageupload->enableImageGallery();
-        
-        $this->form->addFields( [new TLabel('Image Cropper')], [$imagecropper] );
-        $this->form->addFields( [new TLabel('Image Uploader')],[$imageupload] );
-        
+
+        $this->form->addFields([new TLabel('Image Cropper')], [$imagecropper]);
+        $this->form->addFields([new TLabel('Image Uploader')], [$imageupload]);
+
         $this->form->addAction('Show', new TAction(array($this, 'onShow')), 'far:check-circle green');
-        
+
         // wrap the page content using vertical box
         $vbox = new TVBox;
         $vbox->style = 'width: 100%';
@@ -42,16 +42,16 @@ class EditorImagem extends TPage
         $vbox->add($this->form);
         parent::add($vbox);
     }
-    
+
     public static function onChange($param)
     {
-        new TMessage('info', '<b>onChange</b><br>'.str_replace(',', '<br>', json_encode($param)));
+        new TMessage('info', '<b>onChange</b><br>' . str_replace(',', '<br>', json_encode($param)));
     }
-    
+
     public static function onShow($param)
     {
         // show the message
-        new TMessage('info', '<b>Image Crop</b>: tmp/'. $param['imagecropper'] . '<br>'.
-                            ' <b>Image Upload</b>: tmp/' . $param['imageupload']);
+        new TMessage('info', '<b>Image Crop</b>: tmp/' . $param['imagecropper'] . '<br>' .
+            ' <b>Image Upload</b>: tmp/' . $param['imageupload']);
     }
 }
