5cabe3243075d06364b98a9fc80f5729044c862b
LucasBortoloti <bortoloti91@gmail.com>
2024-02-20 10:41:51 -0300

    20/02

--- app/control/apresentacao/EditorTexto.php
+++ app/control/apresentacao/EditorTexto.php
@@ -1,31 +1,31 @@
 <?php
 
 use Adianti\Control\TPage;
+use MarceloNees\TTinyMceAdianti\TTinyMceEditor\TTinyMceEditor;
 
 class EditorTexto extends TPage
 {
     private $form;
-    
+
     /**
      * Page constructor
      */
     function __construct()
     {
         parent::__construct();
-        
+
         // create the form
         $this->form = new BootstrapFormBuilder('my_html_form');
-        $this->form->setFormTitle( ('Editor de Texto ') );
-        
+        $this->form->setFormTitle(('Editor de Texto '));
+
         // create the form fields
-        $html = new THtmlEditor('html_text');
-        $html->setSize( '100%', 200);
-        $html->setOption('placeholder', 'type here...');
-        
-        $this->form->addFields( [$html] );
+        $html = new TTinyMceEditor('html_text');
+        $html->setSize('100%', 200);
+
+        $this->form->addFields([$html]);
         $this->form->addAction('Show', new TAction(array($this, 'onShow')), 'far:check-circle blue');
         $this->form->addAction('Insert text', new TAction(array($this, 'onInsert')), 'fa:plus green');
-        
+
         // wrap the page content using vertical box
         $vbox = new TVBox;
         $vbox->style = 'width: 100%';
@@ -33,20 +33,17 @@ class EditorTexto extends TPage
         $vbox->add($this->form);
         parent::add($vbox);
     }
-    
+
     public static function onInsert($param)
     {
-        THtmlEditor::insertText('my_html_form', 'html_text', 'São Paulo FC, maior do Brasil ! Três mundiais e Três Libertadores.');
+        TTinyMceEditor::insertText('my_html_form', 'html_text', 'São Paulo FC, maior do Brasil ! Três mundiais e Três Libertadores.');
     }
-    
-    /**
-     * Show the form content
-     */
+
     public function onShow($param)
     {
         $data = $this->form->getData();
         $this->form->setData($data); // put the data back to the form
-        
+
         // show the message
         new TMessage('info', $data->html_text);
     }
