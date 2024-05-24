cb5543ad29d02cd2e4631d5b793de5ce74c2f5f9
LucasBortoloti <bortoloti91@gmail.com>
2024-05-24 08:49:10 -0300

    24/05

--- app/control/apresentacao/GraficoSinistro.class.php
+++ app/control/apresentacao/GraficoSinistro.class.php
@@ -1,6 +1,7 @@
 <?php
 
 use Adianti\Control\TPage;
+use Adianti\Widget\Dialog\TMessage;
 use Adianti\Widget\Form\TLabel;
 use Adianti\Widget\Wrapper\TDBCombo;
 use Adianti\Widget\Wrapper\TDBUniqueSearch;
@@ -10,6 +11,7 @@ class GraficoSinistro extends TPage
     protected $form;     // registration form
     protected $datagrid; // listing
     protected $pageNavigation;
+
 
     use Adianti\Base\AdiantiStandardListTrait;
 
@@ -60,6 +62,7 @@ class GraficoSinistro extends TPage
         // $date_to->setMask('dd/mm/yyyy');
 
         $this->form->addAction('Gerar', new TAction(array($this, 'onGenerate')), 'fa:download blue');
+        // $this->form->addAction('Gerar em PDF', new TAction(array($this, 'onGeneratePDF')), 'fa:download blue');
 
         $table = new TTable;
         $table->border = 0;
@@ -88,17 +91,17 @@ class GraficoSinistro extends TPage
         $sinistro = TTransaction::get();
 
         $query = "SELECT
-                s.descricao,
-                b.nome AS bairro_nome,
-                count(*) as QTDE
-              FROM
-                ocorrencia o
-              LEFT JOIN
-                sinistro s ON s.id = o.sinistro_id
-              LEFT JOIN vigepi.bairro b on b.id = o.bairro_id
-              WHERE
-                o.{$pesquisa} >= '{$date_from}' AND
-                o.{$pesquisa} <= '{$date_to}'";
+               s.descricao,
+               b.nome AS bairro_nome,
+               count(*) as QTDE
+             FROM
+               ocorrencia o
+             LEFT JOIN
+               sinistro s ON s.id = o.sinistro_id
+             LEFT JOIN vigepi.bairro b on b.id = o.bairro_id
+             WHERE
+               o.{$pesquisa} >= '{$date_from}' AND
+               o.{$pesquisa} <= '{$date_to}'";
 
         if (!empty($bairro_id)) {
             $query .= " AND o.bairro_id = '{$bairro_id}'";
@@ -135,4 +138,106 @@ class GraficoSinistro extends TPage
 
         parent::add($div);
     }
+
+    // function onGeneratePDF()
+    // {
+    //     try {
+    //         $html = new THtmlRenderer('app/resources/google_pie_chart.html');
+
+    //         $data = $this->form->getData();
+    //         $date_from = $data->date_from;
+    //         $date_to = $data->date_to;
+    //         $bairro_id = $data->bairro_id;
+    //         $pesquisa = $data->pesquisa;
+
+    //         $this->form->setData($data);
+
+    //         TTransaction::open('defciv');
+
+    //         $sinistro = TTransaction::get();
+
+    //         $query = "SELECT
+    //            s.descricao,
+    //            b.nome AS bairro_nome,
+    //            count(*) as QTDE
+    //          FROM
+    //            ocorrencia o
+    //          LEFT JOIN
+    //            sinistro s ON s.id = o.sinistro_id
+    //          LEFT JOIN vigepi.bairro b on b.id = o.bairro_id
+    //          WHERE
+    //            o.{$pesquisa} >= '{$date_from}' AND
+    //            o.{$pesquisa} <= '{$date_to}'";
+
+
+    //         if (!empty($bairro_id)) {
+    //             $query .= " AND o.bairro_id = '{$bairro_id}'";
+    //         }
+
+    //         $query .= " GROUP BY s.descricao ORDER BY s.descricao;";
+
+    //         $colunas = $sinistro->query($query);
+
+    //         // var_dump($colunas);
+
+    //         $dados[] = ['Sinistro', 'Quantidade'];
+
+    //         foreach ($colunas as $coluna) {
+    //             $dados[] = [$coluna['descricao'], (float)$coluna['QTDE']];
+    //         }
+
+    //         $div = new TElement('div');
+    //         $div->id = 'container';
+    //         $div->style = 'width:1555px;height:1150px';
+    //         $div->add($html);
+
+    //         $date_from_formatado = date('d/m/Y', strtotime($date_from));
+    //         $date_to_formatado = date('d/m/Y', strtotime($date_to));
+
+    //         $html->enableSection('main', array(
+    //             'data' => json_encode($dados),
+    //             'width' => '100%',
+    //             'height' => '1000px',
+    //             'title'  => "Sinistros: {$date_from_formatado} até {$date_to_formatado}, Bairro: {$coluna['bairro_nome']}"
+    //         ));
+
+    //         echo json_encode($dados);
+
+    //         $vbox = new TVBox;
+    //         $vbox->style = 'width: 100%';
+    //         $vbox->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+    //         $vbox->add($this->html);
+    //         parent::add($vbox);
+
+    //         // $contents = $html->getContents();
+    //         $contents = $html->getContents();
+
+    //         $options = new \Dompdf\Options();
+    //         $options->setChroot(getcwd());
+
+    //         // converts the HTML template into PDF
+    //         $dompdf = new \Dompdf\Dompdf($options);
+    //         $dompdf->loadHtml($contents);
+    //         $dompdf->setPaper('A4', 'portrait');
+    //         $dompdf->render();
+
+    //         // write and open file
+    //         file_put_contents('app/output/document.pdf', $dompdf->output());
+
+    //         // open window to show pdf
+    //         $window = TWindow::create(('Document HTML->PDF'), 0.8, 0.8);
+    //         $object = new TElement('object');
+    //         $object->data  = 'app/output/grafico.pdf';
+    //         $object->type  = 'application/pdf';
+    //         $object->style = "width: 100%; height:calc(100% - 10px)";
+    //         $object->add('O navegador não suporta a exibição deste conteúdo, <a style="color:#007bff;" target=_newwindow href="' . $object->data . '"> clique aqui para baixar</a>...');
+
+    //         $window->add($object);
+    //         $window->show();
+
+    //         TTransaction::close();
+    //     } catch (Exception $e) {
+    //         new TMessage('error', $e->getMessage());
+    //     }
+    // }
 }
